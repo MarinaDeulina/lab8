5c2e1e4fceee490c77649ade3cdb9b64a9ac0739
Marina Deulina <mail@mail.com>
2018-03-26 00:58:39 +0900

    task22

--- lab8.cpp
+++ lab8.cpp
@@ -1,19 +1,41 @@
 #include <iostream>
 #include <dirent.h>
 #include <sys/types.h>
+#include <sys/stat.h>
+#include <string.h>
 
 using namespace std;
+long long size,count,fcount;
+
+void dfs(){
+DIR* dp = opendir(".");
+	struct dirent *res;
+
+	while ((res = readdir(dp)) != NULL) {
+
+        if (strcmp(res->d_name,".")==0 || strcmp(res->d_name,"..")==0)
+            continue;
+        struct stat info;
+        stat(res->d_name, &info);
+
+    	if (S_ISDIR(info.st_mode)){
+            cout << " DIR" <<  "\n";
+            fcount++;
+            }
+    	else{
+            size+= info.st_size;
+            count++;
+    	}
+        cout << info.st_size<< " " << res->d_name << "\n";
+	}
+
+    closedir(dp);
+    }
+
 
 int main()
 {
-    DIR* dp = opendir(".");
-    struct dirent *res;
-
-    while ((res = readdir(dp)) != NULL) {
-        cout << res->d_name << "\n";
-    }
-    closedir(dp);
-
-
+	dfs();
+    cout << count << " File, " << fcount << " Dir, " << size << " bytes\n";
     return 0;
 }
